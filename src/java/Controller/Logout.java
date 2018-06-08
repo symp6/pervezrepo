fd0bd369a2304a6c827c39d6333e902aca35f55d
symp6 <jellurrhaman@gmail.com>
2018-06-09 04:52:19 +0600

    3rd commit

---
+++ src/java/Controller/Logout.java
@@ -0,0 +1,45 @@
+
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+public class Logout extends HttpServlet {
+
+    
+   
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        HttpSession s = request.getSession();
+        
+        s.removeAttribute("home");
+        s.invalidate();
+        
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+ 
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
